7bc7182e7fda14b103d622f6bb1a3d40f69c93df
Joseph <josephulloa1120@hotmail.com>
2023-06-21 16:01:16 -0600

    practica

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 // Inserte el código aquí
+function horaDeHoy() {
+  let today = new Date();
+  let hora = today.getHours();
+  let minutos = today.getMinutes();
+  let segundos = today.getSeconds();
+
+  return hora + ":" + minutos + ":" + segundos;
+}
+
+function Actualizar() {
+  let horaElemento = document.querySelector(".Hora");
+  horaElemento.textContent = horaDeHoy();
+}
+
+let intervalID = setInterval(Actualizar, 1000);
+
+
+
+// const imprimirUsuarios = () => {
+//   const usuarios = obtenerUsuarios();
+//   usuarios
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .catch(Error);
+// };
+
+
+
+
 
