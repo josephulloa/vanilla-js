5536dd2f9db78c3c5fe3b35807efd02d79ea73a8
Joseph <josephulloa1120@hotmail.com>
2023-06-21 13:09:21 -0600

    ejercicios

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,36 @@
 // Inserte el código aquí
+ 
+//UNO
+let Arreglo = 
+    [1, 2, 3,]
+
+function clonarArreglo(NuevoArreglo){
+   
+Arreglo= [...NuevoArreglo]
+
+ return Arreglo
+
+} 
+
+ //DOS
+function clonarObjeto (original){
+return original
+
+}
+
+const original = {persona: "Jose", ciudad: "barranca", edad: 22 }
+
+const nuevoClon = clonarObjeto (original)
+
+console.log(nuevoClon)
+console.log( original === nuevoClon )
 
 
+//TRES
+function sumar (...suma){ 
+return suma.reduce((total, valor ) => {return total + valor }, 0)
+
+}
 
 // NO MODIFICAR
-export { clonarArreglo, clonarObjeto, sumar, saludar };
+export { clonarArreglo, clonarObjeto, sumar, saludar }
