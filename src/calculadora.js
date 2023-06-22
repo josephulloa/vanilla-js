2d5971266a02ca0d8c7e554bae8be471d58db637
Joseph <josephulloa1120@hotmail.com>
2023-06-22 16:13:55 -0600

    calculadora

---
+++ src/calculadora.js
@@ -0,0 +1,19 @@
+
+let dividir = (num1, num2) => {
+    return num1 / num2
+   }
+
+let multiplicar= (num1, num2) => {
+    return num1*num2
+   }
+
+let restar= (num1, num2) => {
+    return num1-num2
+   }
+
+let sumar= (num1, num2) => {
+    return num1+num2
+    }
+ let calculadora ={dividir, multiplicar, restar, sumar}  
+   
+ export {calculadora}
