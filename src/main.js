1fd8a7df6dc699994ab4eb7c391d51f1c69b6fcf
Joseph <josephulloa1120@hotmail.com>
2023-06-23 16:06:04 -0600

    proyectoReloj

---
+++ src/main.js
@@ -0,0 +1,20 @@
+function horaDeHoy() {
+  let today = new Date();
+  let hora = today.getHours();
+  let minutos = today.getMinutes();
+  let segundos = today.getSeconds();
+
+  return hora + ":" + minutos + ":" + segundos;
+
+
+}
+  
+
+function Actualizar() {
+  let horaElemento = document.querySelector(".Hora");
+  horaElemento.textContent = horaDeHoy();
+}
+
+let intervalID = setInterval(Actualizar, 1000);
+
+
